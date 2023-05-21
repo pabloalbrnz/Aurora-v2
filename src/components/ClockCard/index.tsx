0c91770c4c70bcd826994007f61416245e34e3ec
pulse <pabloalbernazrincon@gmail.com>
2023-05-21 20:50:28 -0300

    Hotfix Clock

--- src/components/ClockCard/index.tsx
+++ src/components/ClockCard/index.tsx
@@ -18,20 +18,12 @@ const months = [
 const date = new Date();
 const month = date.getMonth();
 
-function time() {
-  let hour = date.getHours();
-  let min = date.getMinutes();
-
-  console.log(hour, min);
-
-  let hourS: string, minS: string;
-
-  if (hour < 10) hourS = "0" + hour;
-  else hourS = hour.toString();
-  if (min < 10) minS = "0" + min;
-  else minS = min.toString();
-
-  return `${hourS}:${minS}`;
+function Clock() {
+  return (
+    <span className="hour-now">
+      {date.toLocaleTimeString().substring(0, 5)}
+    </span>
+  );
 }
 
 export interface IClockCardPropos {}
@@ -39,11 +31,7 @@ export interface IClockCardPropos {}
 export function ClockCard({}) {
   return (
     <div className="clock-wrapper">
-      <span className="hour-now">
-        {setInterval(() => {
-          time();
-        })}
-      </span>
+      {Clock()}
       <div className="clock-info-wrapper">
         <span className="weekday">
           {date.toLocaleDateString("en", { weekday: "long" })}
