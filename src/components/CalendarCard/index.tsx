301e77d446c8199fc86aa9ce559023d01a3054cc
pulse <pabloalbernazrincon@gmail.com>
2023-05-25 15:07:33 -0300

    Calendar Logic

--- src/components/CalendarCard/index.tsx
+++ src/components/CalendarCard/index.tsx
@@ -1,25 +1,66 @@
+import { useState } from "react";
 import "./style.css";
+import { useDate } from "./useCalendarCard";
+import { CaretLeft, CaretRight } from "@phosphor-icons/react";
 
 export interface ICalendarCardProps {}
 
 const date = new Date();
 
+const [nav, setNav] = useState(0);
+const { days, dateDisplay } = useDate(nav);
+
+function CalendarHeader() {
+  return (
+    <div className="calendar-header">
+      <div>
+        <CaretLeft
+          onClick={() => setNav(nav - 1)}
+          className="calendar-prev-button"
+          weight="bold"
+          size="16px"
+        />
+        <span className="calendar-month-text">{dateDisplay}</span>
+        <CaretRight
+          onClick={() => setNav(nav + 1)}
+          className="calendar-next-button"
+          weight="bold"
+        />
+      </div>
+    </div>
+  );
+}
+
+function CalendarDays() {
+  return (
+    <div className="calendar-days">
+      {days.map((d, index) => (
+        <div
+          className={`day ${d.value === "padding" ? "padding" : ""} ${
+            d.isCurrentDay ? "currentDay" : ""
+          }`}
+        >
+          {d.value === "padding" ? "" : d.value}
+        </div>
+      ))}
+    </div>
+  );
+}
+
 export function CalendarCard({}: ICalendarCardProps) {
   return (
     <div className="calendar-wrapper">
-      <div className="today-month">
-        <div className="button-wrapper">
-          <button className="prev-button">{"<"}</button>
-        </div>
-        <span className="today-month-str">
-          {`${date.toLocaleDateString("en", { month: "long" })} ${date.getFullYear()}`}
-        </span>
-        <div className="button-wrapper">
-          <button className="next-button">{">"}</button>
-        </div>
+      <CalendarHeader />
+      <div className="calendar-weekdays">
+        <span>Sun</span>
+        <span>Mon</span>
+        <span>Tue</span>
+        <span>Wed</span>
+        <span>Thu</span>
+        <span>Fri</span>
+        <span>Sat</span>
       </div>
-      <div className="weekday"></div>
-      <div className="month-days"></div>
+      <CalendarDays />
     </div>
   );
 }
---
+++ src/components/CalendarCard/useCalendarCard.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+
+export const useDate = (nav) => {
+  const [dateDisplay, setDateDisplay] = useState("");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const weekdays = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const dt = new Date();
+
+    if (nav !== 0) {
+      dt.setMonth(new Date().getMonth() + nav);
+    }
+
+    const day = dt.getDate();
+    const month = dt.getMonth();
+    const year = dt.getFullYear();
+
+    const firstDayOfMonth = new Date(year, month, 1);
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    const dateString = firstDayOfMonth.toLocaleDateString("en-us", {
+      weekday: "long",
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    });
+
+    setDateDisplay(
+      `${dt.toLocaleDateString("en-us", { month: "long" })} ${year}`
+    );
+    const paddingDays = weekdays.indexOf(dateString.split(", ")[0]);
+
+    const daysArr = [];
+
+    for (let i = 1; i <= paddingDays + daysInMonth; i++) {
+      const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+
+      if (i > paddingDays) {
+        daysArr.push({
+          value: i - paddingDays,
+          isCurrentDay: i - paddingDays === day && nav === 0,
+          date: dayString,
+        });
+      } else {
+        daysArr.push({
+          value: "padding",
+          isCurrentDay: false,
+          date: "",
+        });
+      }
+    }
+
+    setDays(daysArr);
+  }, [nav]);
+
+  return {
+    days,
+    dateDisplay,
+  };
+};
